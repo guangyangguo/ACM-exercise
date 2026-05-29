dd369c9d4635da4dd3fd36b2252e54f6bddc27d7
ggy <2430177075@qq.com>
2026-05-29 09:30:23 +0800

    每日一题：3300. 替换为数位和以后的最小元素

---
+++ acm-mode/src/meiriyiti/q3300tihuanweishuweiheyihoudezuixiaoyuansu.java
@@ -0,0 +1,33 @@
+package meiriyiti;
+
+import java.util.Arrays;
+
+public class q3300tihuanweishuweiheyihoudezuixiaoyuansu {
+
+    public static void main(String[] args) {
+        int[] nums = {10,12,13,14};
+        System.out.println(minElement(nums));
+    }
+
+
+    /*
+    * 简单题
+    * */
+    public static int minElement(int[] nums){
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int he = gethe(nums[i]);
+            res = Math.min(res, he);
+        }
+        return res;
+    }
+    public static int gethe(int num){
+        int res = 0;
+        while (num >= 10){
+            res += num % 10;
+            num /= 10;
+        }
+        res += num;
+        return res;
+    }
+}
