35766da1df01e1bb5af63aa2b54d5a82fabc8489
ggy <2430177075@qq.com>
2026-05-31 09:12:07 +0800

    每日一题：2126. 摧毁小行星

---
+++ acm-mode/src/meiriyiti/q2126cuihuixiaoxingxing.java
@@ -0,0 +1,28 @@
+package meiriyiti;
+
+import java.util.Arrays;
+
+public class q2126cuihuixiaoxingxing {
+
+    public static void main(String[] args) {
+        int[] asteroids = {3,9,19,5,21};
+        System.out.println(asteroidsDestroyed(10, asteroids));
+    }
+
+
+    /*
+    * 第一个直觉的解法，注意用long
+    * */
+    public static boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        long res = mass;
+        Arrays.sort(asteroids);
+        for (int i = 0; i < asteroids.length; i++) {
+            if (mass < asteroids[i]){
+                return false;
+            }
+            res += asteroids[i];
+        }
+        return true;
+    }
+
+}
