c83233fe625fdd0a67bd24e93588c38ea5d0a85e
ggy <2430177075@qq.com>
2026-06-01 21:14:11 +0800

    每日一题：2144. 打折购买糖果的最小开销

---
+++ acm-mode/src/meiriyiti/q2144dazhegoumaitangguodezuixiaokaixiao.java
@@ -0,0 +1,24 @@
+package meiriyiti;
+
+import java.util.Arrays;
+
+public class q2144dazhegoumaitangguodezuixiaokaixiao {
+
+    public static void main(String[] args) {
+        int[] cost = {1,2,3};
+        System.out.println(minimumCost(cost));
+    }
+
+    public static int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int sumcost = 0;
+        for(int i = cost.length - 1;i >= 0;--i){
+            //第一贵、第二贵、第三贵的糖果。。。
+            //对于需要赠送的糖果的位置会跳过，不计入总成本
+            if((cost.length - 1 - i) % 3 != 2){
+                sumcost += cost[i];
+            }
+        }
+        return sumcost;
+    }
+}
